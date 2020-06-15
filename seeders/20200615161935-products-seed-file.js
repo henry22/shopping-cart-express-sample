8f35806427f6074612dc71068a091687decacb70
henry22 <henry811010@gmail.com>
2020-06-16 00:46:21 +0800

    add seeders

---
+++ seeders/20200615161935-products-seed-file.js
@@ -0,0 +1,28 @@
+'use strict';
+const faker = require('faker')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Products',
+      Array.from({ length: 10 }).map((item, index) =>
+        ({
+          id: index + 1,
+          name: faker.commerce.product() + '/' + faker.commerce.productName(),
+          price: faker.commerce.price(),
+          image: faker.image.imageUrl(),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+    ), {})
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
